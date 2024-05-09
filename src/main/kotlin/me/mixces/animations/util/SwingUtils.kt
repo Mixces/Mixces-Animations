60223d5fb5d028a0fa361b562b9f00b0be05c3d8
Mixces <aar.hcms@gmail.com>
2024-05-08 21:29:56 -0500

    Kotlin fun

--- src/main/kotlin/me/mixces/animations/util/SwingUtils.kt
+++ src/main/kotlin/me/mixces/animations/util/SwingUtils.kt
@@ -7,8 +7,9 @@ import me.mixces.animations.mixin.accessor.EntityLivingBaseInvoker
 
 fun swingItem(thePlayer: EntityPlayerSP) {
     val armSwingAnimationEnd = (thePlayer as EntityLivingBaseInvoker).invokeGetArmSwingAnimationEnd()
-    if (!thePlayer.isSwingInProgress || thePlayer.swingProgressInt < 0 || thePlayer.swingProgressInt >= armSwingAnimationEnd / 2) {
+    if (!thePlayer.isSwingInProgress || thePlayer.swingProgressInt !in 0 until armSwingAnimationEnd / 2) {
         thePlayer.swingProgressInt = -1
         thePlayer.isSwingInProgress = true
     }
+
 }
