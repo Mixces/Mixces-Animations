3aaa5fefcb9735a36b096a640bb2155eaa4e51a2
Mixces <aar.hcms@gmail.com>
2024-05-08 21:34:38 -0500

    Add nullability check (more Kotlin fun)

--- src/main/kotlin/me/mixces/animations/util/SwingUtils.kt
+++ src/main/kotlin/me/mixces/animations/util/SwingUtils.kt
@@ -6,7 +6,7 @@ import net.minecraft.client.entity.EntityPlayerSP
 import me.mixces.animations.mixin.accessor.EntityLivingBaseInvoker
 
 fun swingItem(thePlayer: EntityPlayerSP) {
-    val armSwingAnimationEnd = (thePlayer as EntityLivingBaseInvoker).invokeGetArmSwingAnimationEnd()
+    val armSwingAnimationEnd = (thePlayer as? EntityLivingBaseInvoker)?.invokeGetArmSwingAnimationEnd() ?: return
     if (!thePlayer.isSwingInProgress || thePlayer.swingProgressInt !in 0 until armSwingAnimationEnd / 2) {
         thePlayer.swingProgressInt = -1
         thePlayer.isSwingInProgress = true
