113b1142b2a7f84d499532eddb755064c4aacfc8
Mixces <aar.hcms@gmail.com>
2024-06-01 22:03:53 -0500

    Simplify old projectile feature!

--- src/main/java/me/mixces/animations/mixin/RenderSnowballMixin.java
+++ src/main/java/me/mixces/animations/mixin/RenderSnowballMixin.java
@@ -1,23 +1,13 @@
 package me.mixces.animations.mixin;
 
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.entity.Render;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderSnowball;
-import net.minecraft.entity.Entity;
 import me.mixces.animations.config.MixcesAnimationsConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = RenderSnowball.class)
-public abstract class RenderSnowballMixin <T extends Entity> extends Render<T> {
-
-    protected RenderSnowballMixin(RenderManager renderManager) {
-        super(renderManager);
-    }
+public abstract class RenderSnowballMixin {
 
     @ModifyArg(
             method = "doRender",
@@ -46,19 +36,6 @@ public abstract class RenderSnowballMixin <T extends Entity> extends Render<T> {
     )
     private float mixcesAnimations$useProperCameraView(float angle) {
         return (MixcesAnimationsConfig.INSTANCE.getOldProjectiles() && MixcesAnimationsConfig.INSTANCE.enabled ? -1F : 1F) * angle;
-    }
-
-    @Inject(
-            method = "doRender",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/renderer/entity/RenderSnowball;bindTexture(Lnet/minecraft/util/ResourceLocation;)V"
-            )
-    )
-    private void mixcesAnimations$translateProjectile(T entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo ci) {
-        if (MixcesAnimationsConfig.INSTANCE.getOldProjectiles() && MixcesAnimationsConfig.INSTANCE.enabled) {
-            GlStateManager.translate(0.0F, 0.25F, 0.0F);
-        }
     }
 
 }
